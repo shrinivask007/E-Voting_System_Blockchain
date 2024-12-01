5239e78306a7f1025afd1e3e8b970e6c3e240169
Shrinivas Kathare <shrinivassk18@gmail.com>
2024-12-01 19:53:09 +0530

    Updated

--- server/routes/addUser.js
+++ server/routes/addUser.js
@@ -1,5 +1,5 @@
-// routes/addUser.js
 const express = require('express');
+const bcrypt = require('bcrypt'); // Import bcrypt
 const router = express.Router();
 const User = require('../models/User'); // Ensure correct path to the User model
 
@@ -7,16 +7,20 @@ const User = require('../models/User'); // Ensure correct path to the User model
 router.post('/', async (req, res) => {
     const { firstName, lastName, email, idNumber, password } = req.body;
 
-    // Create new user instance
-    const newUser = new User({
-        firstName,
-        lastName,
-        email,
-        idNumber,
-        password
-    });
-
     try {
+        // Hash the password before saving
+        const saltRounds = 10; // Recommended value is 10
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        // Create new user instance with the hashed password
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            idNumber,
+            password: hashedPassword // Store the hashed password
+        });
+
         // Save user to MongoDB
         const savedUser = await newUser.save();
         res.json(savedUser); // Respond with the saved user data
